92a043c2aa067f17bcd5882f6229fbc0c9597110
Eddie <eddie@edwinfairchild.com>
2022-01-04 04:15:16 -0600

    Revert "just addings TODOs in header files"
    This reverts commit dd526f73b72b561bbf03b52d80be6f5ac1b80308.

--- uCShell_Config.h
+++ uCShell_Config.h
@@ -3,33 +3,9 @@
 
 /* TODO :
  *
- * The  dependency on my own printMsg should be removed
- * should have the user register a printf like function
+ * BUG : not checking for max message size
  *
  */
-
-/* TODO :
- *
- * Move defines in uCShell.h into this Config file as the
- * only place the user should be making changes
- *
- */
-
-/* TODO :
- *
- * Give the user options to colorize outputs if their
- * terminal viewer suports it ex: Putty | MobaTerm
- *
- */
-
-/* TODO :
- *
- * Eventaully a python based "terminal" where I have the
- * freedom to show the user register values in a GUI
- *
- */
-
-
 
 
 #endif
