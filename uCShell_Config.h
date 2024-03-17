9e07acf353dc332c995bd41a37f150b218bc6f05
Eddie <eddie@edwinfairchild.com>
2024-03-17 12:57:16 -0500

    add macro to header

--- uCShell_Config.h
+++ uCShell_Config.h
@@ -3,6 +3,9 @@
 
 // bugs still exist when runing uCShell in a PC environment
 #define USING_DESKTOP 0
+// When retargeting printf on mcu some extra code is needed
+// NOTE: this does not retatget printf for you, it just lets uCShell know that you did
+#define RETARGET_PRINTF 1
 // not all terminals support color codes (Mobaterm and Putty do)
 #define USE_COLORS 1
 /* TODO :
